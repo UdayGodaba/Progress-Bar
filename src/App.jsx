f3750734d878736461b1e1faa2f5f858417dc721
UdayGodaba <godabauday@gmail.com>
2024-05-21 16:42:39 +0530

    ProgressBar Logic and CSS done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import ProgressBar from "./components/ProgressBar";
 
 function App() {
-  return <></>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((prevVal) => Math.min(prevVal + 1, 100));
+    }, 100);
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="header">Progress Bar</div>
+      <ProgressBar value={value} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
-
-const ProgressBar = () => {
-  return <div>ProgressBar</div>;
+/* eslint-disable react/prop-types */
+const ProgressBar = ({ value = 0 }) => {
+  return (
+    <div className="progressBar" style={{ "--value": `${value}%` }}>
+      <span>{value}%</span>
+      <div></div>
+    </div>
+  );
 };
 
 export default ProgressBar;
